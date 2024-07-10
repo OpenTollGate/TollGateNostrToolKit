6930b72bfb7ed542db962e2ba85d3a68efa6e924
Your Name <you@example.com>
2024-07-10 20:01:41 +0200

    Compile secp256k1 first

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Define the installation directories and compiler settings
-LOCAL_INSTALL_DIR="/usr/local"
-MIPS_INSTALL_DIR="/usr/local/mips-linux-gnu"
+LOCAL_INSTALL_DIR="$HOME/usr/local"
+MIPS_INSTALL_DIR="$HOME/usr/local/mips-linux-gnu"
 TOOLCHAIN_PREFIX="mips-linux-gnu"
 PARENT_DIR=".."
 CURRENT_DIR=$(pwd)
+
+# Create installation directories if they don't exist
+mkdir -p $LOCAL_INSTALL_DIR
+mkdir -p $MIPS_INSTALL_DIR
 
 # Source file and output binaries
 SOURCE_FILE="$CURRENT_DIR/sign_event.c"
@@ -22,31 +26,49 @@ SECP256K1_URL="https://github.com/bitcoin-core/secp256k1.git"
 function clone_dependencies() {
     echo "Cloning dependencies..."
     cd $PARENT_DIR
-    
+
     if [ ! -d "openssl" ]; then
-        git clone --depth 1 $OPENSSL_URL openssl
+	git clone --depth 1 $OPENSSL_URL openssl
     else
-        cd openssl
-        git pull
-        cd ..
+	cd openssl
+	git pull
+	cd ..
     fi
-    
+
     if [ ! -d "libcrypto" ]; then
-        git clone --depth 1 $LIBCRYPTO_URL libcrypto
+	git clone --depth 1 $LIBCRYPTO_URL libcrypto
     else
-        cd libcrypto
-        git pull
-        cd ..
+	cd libcrypto
+	git pull
+	cd ..
     fi
-    
+
     if [ ! -d "secp256k1_mips_architecture" ]; then
-        git clone --depth 1 $SECP256K1_URL secp256k1_mips_architecture
+	git clone --depth 1 $SECP256K1_URL secp256k1_mips_architecture
     else
-        cd secp256k1_mips_architecture
-        git pull
-        cd ..
+	cd secp256k1_mips_architecture
+	git pull
+	cd ..
     fi
-    
+
+    cd $CURRENT_DIR
+}
+
+# Function to compile secp256k1 for local architecture (x86_64)
+function compile_secp256k1_for_local() {
+    echo "Compiling secp256k1 for local architecture..."
+    cd $PARENT_DIR/secp256k1_mips_architecture
+    ./autogen.sh
+    ./configure --enable-static --disable-shared \
+		--enable-module-schnorrsig --enable-module-extrakeys
+    make
+
+    if [ $? -eq 0 ]; then
+	echo "Compilation of secp256k1 successful for local architecture."
+    else
+	echo "Failed to compile secp256k1 for local architecture."
+	exit 1
+    fi
     cd $CURRENT_DIR
 }
 
@@ -54,14 +76,17 @@ function clone_dependencies() {
 function compile_for_local() {
     echo "Compiling for local architecture..."
     gcc $SOURCE_FILE -o $LOCAL_BINARY \
-        -I$LOCAL_INSTALL_DIR/include -L$LOCAL_INSTALL_DIR/lib \
-        -lsecp256k1 -lssl -lcrypto
+	-I$PARENT_DIR/secp256k1_mips_architecture/include \
+	-I$LOCAL_INSTALL_DIR/include \
+	-L$PARENT_DIR/secp256k1_mips_architecture/.libs \
+	-L$LOCAL_INSTALL_DIR/lib \
+	-lsecp256k1 -lssl -lcrypto
 
     if [ $? -eq 0 ]; then
-        echo "Compilation successful: $LOCAL_BINARY"
+	echo "Compilation successful: $LOCAL_BINARY"
     else
-        echo "Failed to compile for local architecture."
-        exit 1
+	echo "Failed to compile for local architecture."
+	exit 1
     fi
 }
 
@@ -70,16 +95,16 @@ function compile_openssl_for_mips() {
     echo "Compiling OpenSSL for MIPS architecture..."
     cd $PARENT_DIR/openssl
     ./Configure linux-mips32 --prefix=$MIPS_INSTALL_DIR no-shared no-asm \
-        CC=$TOOLCHAIN_PREFIX-gcc AR=$TOOLCHAIN_PREFIX-ar \
-        RANLIB=$TOOLCHAIN_PREFIX-ranlib LD=$TOOLCHAIN_PREFIX-ld
+		CC=$TOOLCHAIN_PREFIX-gcc AR=$TOOLCHAIN_PREFIX-ar \
+		RANLIB=$TOOLCHAIN_PREFIX-ranlib LD=$TOOLCHAIN_PREFIX-ld
     make
     make install
 
     if [ $? -eq 0 ]; then
-        echo "Compilation of OpenSSL successful for MIPS."
+	echo "Compilation of OpenSSL successful for MIPS."
     else
-        echo "Failed to compile OpenSSL for MIPS architecture."
-        exit 1
+	echo "Failed to compile OpenSSL for MIPS architecture."
+	exit 1
     fi
     cd $CURRENT_DIR
 }
@@ -90,14 +115,15 @@ function compile_secp256k1_for_mips() {
     cd $PARENT_DIR/secp256k1_mips_architecture
     ./autogen.sh
     ./configure --host=mips-linux-gnu --enable-static --disable-shared \
-        CC=$TOOLCHAIN_PREFIX-gcc
+		--enable-module-schnorrsig --enable-module-extrakeys \
+		CC=$TOOLCHAIN_PREFIX-gcc
     make
 
     if [ $? -eq 0 ]; then
-        echo "Compilation of secp256k1 successful for MIPS."
+	echo "Compilation of secp256k1 successful for MIPS."
     else
-        echo "Failed to compile secp256k1 for MIPS architecture."
-        exit 1
+	echo "Failed to compile secp256k1 for MIPS architecture."
+	exit 1
     fi
     cd $CURRENT_DIR
 }
@@ -106,15 +132,17 @@ function compile_secp256k1_for_mips() {
 function compile_for_mips() {
     echo "Compiling for MIPS architecture..."
     $TOOLCHAIN_PREFIX-gcc $SOURCE_FILE -o $MIPS_BINARY \
-        -I$PARENT_DIR/secp256k1_mips_architecture/include -L$PARENT_DIR/secp256k1_mips_architecture/.libs \
-        -I$MIPS_INSTALL_DIR/include -L$MIPS_INSTALL_DIR/lib \
-        -lsecp256k1 -lssl -lcrypto -static
+			  -I$PARENT_DIR/secp256k1_mips_architecture/include \
+			  -I$MIPS_INSTALL_DIR/include \
+			  -L$PARENT_DIR/secp256k1_mips_architecture/.libs \
+			  -L$MIPS_INSTALL_DIR/lib \
+			  -lsecp256k1 -lssl -lcrypto -static
 
     if [ $? -eq 0 ]; then
-        echo "Compilation successful: $MIPS_BINARY"
+	echo "Compilation successful: $MIPS_BINARY"
     else
-        echo "Failed to compile for MIPS architecture."
-        exit 1
+	echo "Failed to compile for MIPS architecture."
+	exit 1
     fi
 }
 
@@ -130,6 +158,7 @@ function generate_checksums() {
 
 # Main execution flow
 clone_dependencies
+compile_secp256k1_for_local
 compile_for_local
 compile_openssl_for_mips
 compile_secp256k1_for_mips
