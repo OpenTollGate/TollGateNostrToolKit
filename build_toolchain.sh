e0a69b78f73a666a9f92177d4156ae5995499034
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-07-15 21:02:58 +0200

    make oldconfig

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -9,7 +9,7 @@ cd $OPENWRT_DIR || { echo "Failed to cd to $OPENWRT_DIR"; exit 1; }
 # WARNING: your configuration is out of sync. Please run make menuconfig, oldconfig or defconfig!
 cp $SCRIPT_DIR/.config $OPENWRT_DIR/.config
 cp $SCRIPT_DIR/feeds.conf $OPENWRT_DIR/feeds.conf
-# make defconfig
+make oldconfig # make defconfig
 
 # Ensure toolchain directory exists
 TOOLCHAIN_DIR="$OPENWRT_DIR/staging_dir/toolchain-mips_24kc_gcc-12.3.0_musl/host"
