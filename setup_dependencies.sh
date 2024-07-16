54593b4b503ee8fb17f44a89eeaa642e5208f901
Your Name <you@example.com>
2024-07-16 15:59:20 +0200

    Added jq

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -15,7 +15,7 @@ fi
 declare -a packages=(
   "build-essential" "libncurses5-dev" "libncursesw5-dev" "git" "python3" 
   "rsync" "file" "wget" "clang" "flex" "bison" "g++" "gawk" "gcc-multilib"
-  "g++-multilib" "gettext" "libssl-dev" "python3-distutils" "unzip" "zlib1g-dev"
+  "g++-multilib" "gettext" "libssl-dev" "python3-distutils" "unzip" "zlib1g-dev" "jq"
 )
 
 # Install necessary dependencies only if they are not already installed
