c177880476b4d0aceac7088e66185b19d58676c8
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-08-05 08:18:01 +0200

    Added nostr_client_relay and libwally

--- build_custom_dependencies.sh
+++ build_custom_dependencies.sh
@@ -29,7 +29,7 @@ check_secp256k1_config
 
 # Install the dependencies from the custom feed
 echo "Installing dependencies from custom feed..."
-./scripts/feeds install libwebsockets libopenssl secp256k1
+./scripts/feeds install libwebsockets libopenssl secp256k1 libwally nostr_client_relay
 
 # Check for feed install errors
 if [ $? -ne 0 ]; then
@@ -43,16 +43,20 @@ check_secp256k1_config
 echo "Building the dependencies..."
 make -j$(nproc) package/libwebsockets/download V=s
 make -j$(nproc) package/secp256k1/download V=s
+make -j$(nproc) package/libwally/download V=s
+make -j$(nproc) package/nostr_client_relay/download V=s
 
 make -j$(nproc) package/libwebsockets/check V=s
 make -j$(nproc) package/secp256k1/check V=s
+make -j$(nproc) package/libwally/check V=s
+make -j$(nproc) package/nostr_client_relay/check V=s
 
 make -j$(nproc) package/libwebsockets/compile V=s
 make -j$(nproc) package/secp256k1/compile V=s
-
+make -j$(nproc) package/libwally/compile V=s
+make -j$(nproc) package/nostr_client_relay/compile V=s
 
 check_secp256k1_config
-
 
 # Build the firmware
 echo "Building the firmware..."
@@ -67,6 +71,8 @@ echo "Finding the generated IPK file..."
 TARGET_DIR="bin/packages/*/*"
 find $TARGET_DIR -name "*secp256k1*.ipk"
 find $TARGET_DIR -name "*libwebsockets*.ipk"
+find $TARGET_DIR -name "*libwally*.ipk"
+find $TARGET_DIR -name "*nostr_client_relay*.ipk"
 
 echo "OpenWrt build completed successfully!"
 
