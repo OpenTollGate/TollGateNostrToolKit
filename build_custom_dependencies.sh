30aecc32dc2713ec79afd991da9060a24980321a
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-08-09 10:14:32 +0200

    Added TollGate to custom dependencies

--- build_custom_dependencies.sh
+++ build_custom_dependencies.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-# Define the function to check if secp256k1 is set to true in .config
-check_secp256k1_config() {
-  if ! grep -q "^CONFIG_PACKAGE_secp256k1=y" .config; then
-    echo "Error: secp256k1 is not set to true in the .config file."
-    exit 1
-  fi
-}
-
 SCRIPT_DIR="$HOME/TollGateNostrToolKit"
 OPENWRT_DIR="$HOME/openwrt"
 cd $OPENWRT_DIR
@@ -19,17 +11,15 @@ cp $SCRIPT_DIR/.config_secp256k1 $OPENWRT_DIR/.config
 cp $SCRIPT_DIR/feeds.conf $OPENWRT_DIR/feeds.conf
 make oldconfig
 
-check_secp256k1_config
 
 # Update the custom feed
 echo "Updating custom feed..."
 ./scripts/feeds update custom
 
-check_secp256k1_config
 
 # Install the dependencies from the custom feed
 echo "Installing dependencies from custom feed..."
-./scripts/feeds install libwebsockets libopenssl secp256k1 libwally nostr_client_relay
+./scripts/feeds install libwebsockets libopenssl secp256k1 libwally nostr_client_relay gltollgate
 
 # Check for feed install errors
 if [ $? -ne 0 ]; then
@@ -37,7 +27,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-check_secp256k1_config
 
 # Build the specific packages
 echo "Building libwebsockets..."
@@ -60,7 +49,11 @@ make -j$(nproc) package/nostr_client_relay/download V=s
 make -j$(nproc) package/nostr_client_relay/check V=s
 make -j$(nproc) package/nostr_client_relay/compile V=s
 
-check_secp256k1_config
+echo "Building gltollgate..."
+make -j$(nproc) package/gltollgate/download V=s
+make -j$(nproc) package/gltollgate/check V=s
+make -j$(nproc) package/gltollgate/compile V=s
+
 
 # Build the firmware
 echo "Building the firmware..."
@@ -77,6 +70,7 @@ find $TARGET_DIR -name "*secp256k1*.ipk"
 find $TARGET_DIR -name "*libwebsockets*.ipk"
 find $TARGET_DIR -name "*libwally*.ipk"
 find $TARGET_DIR -name "*nostr_client_relay*.ipk"
+find $TARGET_DIR -name "*gltollgate*.ipk"
 
 echo "OpenWrt build completed successfully!"
 
