aa1d613a54325ba68191bc44139246b1780a9f84
Your Name <you@example.com>
2024-08-30 17:58:02 +0000

    Added relaylink and signevent

--- build_coordinator.sh
+++ build_coordinator.sh
@@ -38,6 +38,7 @@ rm -f /tmp/my_script_*.timestamp
 
 execute_if_new_day "setup_dependencies.sh"
 execute_if_new_day "clone_openwrt_sdk.sh"
+exit 0
 execute_if_new_day "build_all_dependencies.sh"
 # execute_if_new_day "compile_relay_link.sh"
 # execute_if_new_day "compile_sign_event.sh"
