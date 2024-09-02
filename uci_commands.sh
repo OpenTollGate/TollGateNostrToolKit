73280adc38d41bc3cd3ea015232a4eed635fc4b2
Your Name <you@example.com>
2024-09-02 16:16:10 +0000

    Set DNS server in UCI commands

--- uci_commands.sh
+++ uci_commands.sh
@@ -9,8 +9,11 @@ uci set wireless.default_radio0.network='lan'        # Bind the Wi-Fi to the LAN
 uci set wireless.default_radio0.disable='0'        # Bind the Wi-Fi to the LAN network
 uci commit wireless
 
-/etc/init.d/network restart
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+/etc/init.d/dnsmasq restart
 
+/etc/init.d/network restart
 
 rootpassword="tollgate"
 /bin/passwd root << EOF
