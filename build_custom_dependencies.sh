532513fd32d1b3471b96eae4ccd8d8c9122edd81
Your Name <you@example.com>
2024-09-06 13:05:42 +0000

    Install steps were wrong. Create a dedicated script

--- build_custom_dependencies.sh
+++ build_custom_dependencies.sh
@@ -71,41 +71,41 @@ fi
 # Manually install custom files
 echo "Manually installing custom files..."
 CUSTOM_FILES_DIR="$SCRIPT_DIR/files"
+DESTINATION_DIR="$OPENWRT_DIR/package/base-files/files"
 if [ -d "$CUSTOM_FILES_DIR" ]; then
 	# Create necessary directories
-	mkdir -p "$OPENWRT_DIR/files/etc/uci-defaults"
-	mkdir -p "$OPENWRT_DIR/files/usr/local/bin"
-	mkdir -p "$OPENWRT_DIR/files/etc/init.d"
-	mkdir -p "$OPENWRT_DIR/files/etc/rc.d"
-	mkdir -p "$OPENWRT_DIR/files/www/cgi-bin"
-	mkdir -p "$OPENWRT_DIR/files/etc/config/nodogsplash"
-	mkdir -p "$OPENWRT_DIR/files/etc/nodogsplash/htdocs"
-	mkdir -p "$OPENWRT_DIR/files/etc/firewall.nodogsplash"
+	mkdir -p "$DESTINATION_DIR/etc/uci-defaults"
+	mkdir -p "$DESTINATION_DIR/usr/local/bin"
+	mkdir -p "$DESTINATION_DIR/etc/init.d"
+	mkdir -p "$DESTINATION_DIR/etc/rc.d"
+	mkdir -p "$DESTINATION_DIR/www/cgi-bin"
+	mkdir -p "$DESTINATION_DIR/etc/nodogsplash/htdocs"
+	mkdir -p "$DESTINATION_DIR/etc/firewall.nodogsplash"
 
 	# Copy files from the custom directory to the OpenWrt files directory
-	cp "$CUSTOM_FILES_DIR/80_mount_root" "$OPENWRT_DIR/files/etc/uci-defaults/"
-	cp "$CUSTOM_FILES_DIR/first-login-setup" "$OPENWRT_DIR/files/usr/local/bin/"
-	cp "$CUSTOM_FILES_DIR/create_gateway.sh" "$OPENWRT_DIR/files/etc/"
-	cp "$CUSTOM_FILES_DIR/activate_tollgate.sh" "$OPENWRT_DIR/files/etc/"
-	cp "$CUSTOM_FILES_DIR/deactivate_tollgate.sh" "$OPENWRT_DIR/files/etc/"
-	cp "$CUSTOM_FILES_DIR/cgi-bin/"*.sh "$OPENWRT_DIR/files/www/cgi-bin/"
-	cp -r "$CUSTOM_FILES_DIR/etc/config/nodogsplash" "$OPENWRT_DIR/files/etc/config/"
-	cp -r "$CUSTOM_FILES_DIR/etc/nodogsplash/htdocs/"* "$OPENWRT_DIR/files/etc/nodogsplash/htdocs/"
-	cp -r "$CUSTOM_FILES_DIR/etc/firewall.nodogsplash" "$OPENWRT_DIR/files/etc/"
-	cp -r "$CUSTOM_FILES_DIR/etc/nodogsplash" "$OPENWRT_DIR/files/etc/"
+	cp "$CUSTOM_FILES_DIR/80_mount_root" "$DESTINATION_DIR/etc/uci-defaults/"
+	cp "$CUSTOM_FILES_DIR/first-login-setup" "$DESTINATION_DIR/usr/local/bin/"
+	cp "$CUSTOM_FILES_DIR/create_gateway.sh" "$DESTINATION_DIR/etc/"
+	cp "$CUSTOM_FILES_DIR/activate_tollgate.sh" "$DESTINATION_DIR/etc/"
+	cp "$CUSTOM_FILES_DIR/deactivate_tollgate.sh" "$DESTINATION_DIR/etc/"
+	cp "$CUSTOM_FILES_DIR/cgi-bin/"*.sh "$DESTINATION_DIR/www/cgi-bin/"
+	cp -r "$CUSTOM_FILES_DIR/nodogsplash" "$DESTINATION_DIR/etc/config/"
+	cp -r "$CUSTOM_FILES_DIR/etc/nodogsplash/htdocs/"* "$DESTINATION_DIR/etc/nodogsplash/htdocs/"
+	cp -r "$CUSTOM_FILES_DIR/firewall.nodogsplash" "$DESTINATION_DIR/etc/"
+	cp -r "$CUSTOM_FILES_DIR/etc/nodogsplash" "$DESTINATION_DIR/etc/"
 
 	# Set execute permissions
-	chmod +x "$OPENWRT_DIR/files/usr/local/bin/first-login-setup"
-	chmod +x "$OPENWRT_DIR/files/etc/connect_to_gateway.sh"
+	chmod +x "$DESTINATION_DIR/usr/local/bin/first-login-setup"
+	chmod +x "$DESTINATION_DIR/etc/connect_to_gateway.sh"
 
 	# Copy uci_commands.sh and make it run on first boot
-	mkdir -p "$OPENWRT_DIR/files/etc/opkg/"
-	cp "$CUSTOM_FILES_DIR/distfeeds.conf" "$OPENWRT_DIR/files/etc/opkg/distfeeds.conf"
-	cp "$CUSTOM_FILES_DIR/uci_commands.sh" "$OPENWRT_DIR/files/etc/uci-defaults/99-custom-settings"
-	chmod +x "$OPENWRT_DIR/files/etc/uci-defaults/99-custom-settings"
+	mkdir -p "$DESTINATION_DIR/etc/opkg/"
+	cp "$CUSTOM_FILES_DIR/distfeeds.conf" "$DESTINATION_DIR/etc/opkg/distfeeds.conf"
+	cp "$CUSTOM_FILES_DIR/uci_commands.sh" "$DESTINATION_DIR/etc/uci-defaults/99-custom-settings"
+	chmod +x "$DESTINATION_DIR/etc/uci-defaults/99-custom-settings"
 
 	# Directly modify /etc/profile
-	cat << 'EOF' >> "$OPENWRT_DIR/files/etc/profile"
+	cat << 'EOF' >> "$DESTINATION_DIR/etc/profile"
 
 # TollGateNostr first login setup
 if [ ! -f /etc/first_login_done ] && [ "$SSH_TTY" != "" -o "$(tty)" = "/dev/tts/0" ]; then
@@ -114,7 +114,7 @@ fi
 EOF
 
     # Create a startup script
-    cat << 'EOF' > "$OPENWRT_DIR/files/etc/init.d/99-run-first-login"
+    cat << 'EOF' > "$DESTINATION_DIR/etc/init.d/99-run-first-login"
 #!/bin/sh /etc/rc.common
 
 START=99
@@ -126,11 +126,11 @@ start() {
 }
 EOF
 
-    chmod +x "$OPENWRT_DIR/files/etc/init.d/99-run-first-login"
-    ln -sf ../init.d/99-run-first-login "$OPENWRT_DIR/files/etc/rc.d/S99run-first-login"
+    chmod +x "$DESTINATION_DIR/etc/init.d/99-run-first-login"
+    ln -sf ../init.d/99-run-first-login "$DESTINATION_DIR/etc/rc.d/S99run-first-login"
 
     # Create UCI defaults script
-    cat << 'EOF' > "$OPENWRT_DIR/files/etc/uci-defaults/99-first-login-setup"
+    cat << 'EOF' > "$DESTINATION_DIR/etc/uci-defaults/99-first-login-setup"
 #!/bin/sh
 
 [ -f /etc/first_login_done ] || {
@@ -140,7 +140,7 @@ EOF
 exit 0
 EOF
 
-	chmod +x "$OPENWRT_DIR/files/etc/uci-defaults/99-first-login-setup"
+	chmod +x "$DESTINATION_DIR/etc/uci-defaults/99-first-login-setup"
     
 	echo "Custom files copied to OpenWrt files directory"
 else
