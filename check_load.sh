0a0feee61f6be65290d23c29905fe0396d18ac95
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-09-08 15:28:41 +0200

    Maybe this will reduce the setup friction

---
+++ check_load.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt-get -y install bc
+
+# List of VPS servers
+servers=("helsinki" "falkenstein" "nuremberg")
+# servers=("falkenstein" "nuremberg")
+
+# Function to check load average
+check_load() {
+    ssh root@$1.orangesync.tech "uptime | awk '{print \$10}' | cut -d ',' -f1"
+}
+
+# Function to check if file exists
+check_file() {
+    ssh root@$1.orangesync.tech "[ -f /home/username/TollGateNostrToolKit/binaries/openwrt-ath79-nand-glinet_gl-ar300m-nor-squashfs-sysupgrade.bin ] && echo 'exists' || echo 'not exists'"
+}
+
+# Main loop
+while true; do
+    for server in "${servers[@]}"; do
+        load=$(check_load $server)
+        file_status=$(check_file $server)
+        
+        echo "Server $server load: $load"  # Print the load
+        echo "Server $server file status: $file_status"  # Print file status
+        
+        if (( $(echo "$load < 0.5" | bc -l) )) || [ "$file_status" == "exists" ]; then
+            message="Load average: $load, File status: $file_status"
+            notify-send "Alert for server $server" "$message"
+            # Optional: play a sound
+            paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+        fi
+    done
+    sleep 10  # Check every 10 seconds
+done
--- setup_from_scratch.sh
+++ setup_from_scratch.sh
@@ -4,9 +4,6 @@ REPO_DIR="/home/$USERNAME/TollGateNostrToolKit"
 REPO_URL="https://github.com/chGoodchild/TollGateNostrToolKit.git"
 
 
-# Execute the curl script as root
-curl -sSL https://raw.githubusercontent.com/chGoodchild/vps_general_setup/main/setup.sh | sudo bash
-
 # Check if the user already exists
 if id "$USERNAME" &>/dev/null; then
     echo "User $USERNAME already exists."
@@ -18,10 +15,12 @@ fi
 
 # Switch to the new user
 sudo su - $USERNAME <<EOF
-    curl -sSL https://raw.githubusercontent.com/chGoodchild/TollGateNostrToolKit/nostr_client_relay/setup_repo.sh | bash
+     # Execute the curl script as root
+     curl -sSL https://raw.githubusercontent.com/chGoodchild/vps_general_setup/main/setup.sh | sudo bash
+     curl -sSL https://raw.githubusercontent.com/chGoodchild/TollGateNostrToolKit/nostr_client_relay/setup_repo.sh | bash
 
-    # Log in to the new user's shell
-    # exec bash
+     # Log in to the new user's shell
+     # exec bash
 EOF
 
 # Login as the new user
