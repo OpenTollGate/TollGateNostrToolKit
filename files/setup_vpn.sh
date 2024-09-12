99d430d4525fee6c2be2e13e99c71e0bb355e51b
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-09-12 14:31:25 +0200

    Outline for VPN setup scripts

---
+++ files/setup_vpn.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# ~/TollGateNostrToolKit/files/vpn$ ls
+# firewall  firewall.user  network  openvpn  pia_latvia.ovpn
+# Copy files to their respective locations on the router
+#
+# root@192.168.8.1:/etc/openvpn/
+# root@192.168.8.1:/etc/openvpn/
+# root@192.168.8.1:/etc/config/
+# root@192.168.8.1:/etc/config/
+# root@192.168.8.1:/etc/config/
+# root@192.168.8.1:/etc/
+
+# Install Luci app openvpn and dig if they aren't already installed
+# opkg update && opkg install luci-app-openvpn dig
+
+
