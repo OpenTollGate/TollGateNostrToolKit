8290381131ebef420a3d119e15459235ff45e1fc
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-09-29 11:30:39 +0200

    Restart related services

--- files/root/enable_opennds.sh
+++ files/root/enable_opennds.sh
@@ -5,12 +5,6 @@ cd /tmp/ndslog
 wget https://raw.githubusercontent.com/openNDS/openNDS/master/community/themespec/theme_voucher/vouchers.txt
 chmod 744 /usr/lib/opennds/theme_voucher.sh
 
-mv /etc/config/opennds /root/etc_config_opennds
-mv /etc/opennds/config.uci /root/etc_opennds_config.uci
-
-rm /etc/config/opennds /root/etc_config_opennds
-rm /etc/opennds/config.uci /root/etc_opennds_config.uci
-
 touch /etc/config/opennds
 uci add opennds opennds
 
@@ -20,8 +14,11 @@ uci set opennds.@opennds[0].allow_preemptive_authentication=0
 uci set opennds.@opennds[0].enabled=1
 
 uci commit opennds
-service opennds restart
-service opennds enable
-echo "commented out reboot, might need to restart other services"
-# reboot
 
+/etc/init.d/network restart
+/etc/init.d/firewall restart
+/etc/init.d/dnsmasq restart
+/etc/init.d/opennds restart
+service opennds enable
+
+echo "Services restarted. Captive portal should now be functional."
