b67dce3399b79491f22f4a7a6a0f361e9302a053
Your Name <you@example.com>
2024-10-04 17:59:25 +0200

    Upgraded openwrt to v23.05.5

--- clone_openwrt_sdk.sh
+++ clone_openwrt_sdk.sh
@@ -5,7 +5,7 @@ OPENWRT_DIR="$HOME/openwrt"
 # Clone the OpenWrt repository if it doesn't exist
 if [ ! -d "$OPENWRT_DIR" ]; then
   echo "Cloning OpenWrt repository..."
-  git clone --depth 1 --branch v23.05.3 https://github.com/openwrt/openwrt.git $OPENWRT_DIR
+  git clone --depth 1 --branch v23.05.5 https://github.com/openwrt/openwrt.git $OPENWRT_DIR
   if [ $? -ne 0 ]; then
     echo "Failed to clone OpenWrt repository"
     exit 1
