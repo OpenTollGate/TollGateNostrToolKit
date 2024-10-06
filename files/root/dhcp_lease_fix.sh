d316720caf1ce3c9a962c6938f551179a1451387
Your Name <you@example.com>
2024-10-06 18:46:37 +0200

    Reintroduced ipv6 stuff to DHCP_lease fix

--- files/root/dhcp_lease_fix.sh
+++ files/root/dhcp_lease_fix.sh
@@ -28,6 +28,11 @@ wait_for_opennds() {
     return 0
 }
 
+uci set 'network.lan.ipv6=0'
+uci set 'network.wan.ipv6=0'
+uci set 'dhcp.lan.dhcpv6=disabled'
+/etc/init.d/odhcpd disable
+uci commit
 
 uci -q delete dhcp.lan.dhcpv6
 uci -q delete dhcp.lan.ra
