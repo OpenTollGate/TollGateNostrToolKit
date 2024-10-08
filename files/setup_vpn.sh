ec5fe697138284a8b553dfb74870025aee1f5fdf
Your Name <you@example.com>
2024-10-08 22:11:26 +0200

    Reverted to 2c4af36f2b0b2ea3fe977f7044e85ea06e9ae1e5

--- files/setup_vpn.sh
+++ files/setup_vpn.sh
@@ -23,10 +23,13 @@ install_package_if_needed "openvpn-openssl"
 install_package_if_needed "bind-tools"
 
 # Prompt user for username and password
-echo "Please enter your VPN username:"
-read username
-echo "Please enter your VPN password:"
-read -s password
+# echo "Please enter your VPN username:"
+# read username
+# echo "Please enter your VPN password:"
+# read -s password
+
+username="p4523384"
+password="Nostrhackday1"
 
 # Save username and password to auth.txt
 echo -e "$username\n$password" > /etc/openvpn/auth.txt
