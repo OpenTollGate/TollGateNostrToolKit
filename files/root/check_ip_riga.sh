cfb9b35478068d4606c29d88c5f65ea961f99d4a
Your Name <you@example.com>
2024-10-18 15:09:42 +0200

    Squashed commits from tool_for_connecting_to_gateway

---
+++ files/root/check_ip_riga.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Function to get current public IP and its location
+get_ip_location() {
+    local ip=$(curl -s ifconfig.me)
+    local location=$(curl -s "http://ip-api.com/csv/${ip}?fields=country,city" | tr ',' ' ')
+    echo "$location"
+}
+
+# Check if the IP location is in Riga
+ip_location=$(get_ip_location)
+if (echo "$ip_location" | grep -q "Latvia Riga"); then
+    echo "IP location is in Riga: $ip_location"
+else
+    echo "IP location is not in Riga: $ip_location. Restarting VPN..."
+    service openvpn restart
+    /root/./fix_ping.sh
+fi
